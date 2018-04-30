351b98ad56190867ceecb3ae237bd8b321e73bcb
Olwynrose <lauriane.elena.fels@gmail.com>
2018-04-30 19:17:10 +0200

    2nd commit
    page problématique unifiée et fonctionnelle

--- script/temp_encl.js
+++ script/temp_encl.js
@@ -15,12 +15,59 @@ function handler(event) {
     }
   }
 }
+
+
+
+
 $("input.ensavoirplus").click(handler).find(".ensavoirplus").hide();
 
 
+$("#infost div .b_pb_t").attr('id', "selected");
+$("#nav_pbl").hide();
+$("#nav_pbt").show();
 
+$("#article_pb").show();
+$("#liste_mc").hide();
+$("#liste_pb").hide();
 
+$(".b_pb_mc").click(function() {
+  $("#article_pb").hide();
+  $("#liste_mc").show();
+  $("#liste_pb").hide();
 
+  $("#infost div .b_pb_mc").attr('id', "selected");
+  $("#infost div .b_pb_l").attr('id', "");
+  $("#infost div .b_pb_t").attr('id', "");
+
+  $("#nav_pbl").show();
+  $("#nav_pbt").hide();
+});
+
+$(".b_pb_l").click(function() {
+  $("#article_pb").hide();
+  $("#liste_mc").hide();
+  $("#liste_pb").show();
+
+  $("#infost div .b_pb_l").attr('id', "selected");
+  $("#infost div .b_pb_mc").attr('id', "");
+  $("#infost div .b_pb_t").attr('id', "");
+
+  $("#nav_pbl").show();
+  $("#nav_pbt").hide();
+});
+
+$(".b_pb_t").click(function() {
+  $("#article_pb").show();
+  $("#liste_mc").hide();
+  $("#liste_pb").hide();
+
+  $("#infost div .b_pb_t").attr('id', "selected");
+  $("#infost div .b_pb_l").attr('id', "");
+  $("#infost div .b_pb_mc").attr('id', "");
+
+  $("#nav_pbl").hide();
+  $("#nav_pbt").show();
+});
 
 
 
