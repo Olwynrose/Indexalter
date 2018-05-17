3f828281ab4bb8f0bb6924281306b119c7218766
Olwynrose <lauriane.elena.fels@gmail.com>
2018-05-17 13:35:56 +0200

    3rd commit
    Ajustement du script d'affichage de la page problématique

--- script/temp_encl.js
+++ script/temp_encl.js
@@ -22,6 +22,10 @@ function handler(event) {
 $("input.ensavoirplus").click(handler).find(".ensavoirplus").hide();
 
 
+
+ /* GESTION DE L'ONGLET SELECTIONNE */
+
+ // sélection par défaut de l'onglet "Problématique par thème"
 $("#infost div .b_pb_t").attr('id', "selected");
 $("#nav_pbl").hide();
 $("#nav_pbt").show();
@@ -30,20 +34,22 @@ $("#article_pb").show();
 $("#liste_mc").hide();
 $("#liste_pb").hide();
 
-$(".b_pb_mc").click(function() {
+
+$(".b_pb_mc").click(function Affiche_problématiques_par_motclé() {
   $("#article_pb").hide();
   $("#liste_mc").show();
   $("#liste_pb").hide();
 
-  $("#infost div .b_pb_mc").attr('id', "selected");
   $("#infost div .b_pb_l").attr('id', "");
+  $("#infost div .b_pb_mc").attr('id', "selected");
   $("#infost div .b_pb_t").attr('id', "");
 
   $("#nav_pbl").show();
   $("#nav_pbt").hide();
 });
 
-$(".b_pb_l").click(function() {
+
+$(".b_pb_l").click(function Affiche_liste_problématiques() {
   $("#article_pb").hide();
   $("#liste_mc").hide();
   $("#liste_pb").show();
@@ -56,7 +62,8 @@ $(".b_pb_l").click(function() {
   $("#nav_pbt").hide();
 });
 
-$(".b_pb_t").click(function() {
+
+$(".b_pb_t").click(function Affiche_problématiques_par_thème() {
   $("#article_pb").show();
   $("#liste_mc").hide();
   $("#liste_pb").hide();
@@ -68,6 +75,9 @@ $(".b_pb_t").click(function() {
   $("#nav_pbl").hide();
   $("#nav_pbt").show();
 });
+
+
+
 
 
 
